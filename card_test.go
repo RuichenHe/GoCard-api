717663850c11fcd61eb54e13c9e1c407ad38a1aa
Haimeng <kylehe970128@gmail.com>
2022-07-08 03:20:00 -0700

    Add sort function

--- card_test.go
+++ card_test.go
@@ -17,3 +17,19 @@ func TestNew(t *testing.T) {
 		t.Error("Wrong number of cards!")
 	}
 }
+
+func TestDefaultSort(t *testing.T) {
+	cards := New(DefaultSort)
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("Expect Ace of Spades to be the first one!recieved:", cards[0])
+	}
+}
+
+func TestSort(t *testing.T) {
+	cards := New(Sort(Less))
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("Expect Ace of Spades to be the first one!recieved:", cards[0])
+	}
+}
